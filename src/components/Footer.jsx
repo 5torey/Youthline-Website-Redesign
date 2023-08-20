2c5a1fd1e6ec1aa6c244359b3b25a8e610b3f229
5torey <india.storey@gmail.com>
2023-08-20 21:21:56 +1200

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
 import Logo from '../../public/assets/ytlogoblack.svg'
 import {Link} from 'react-router-dom'
+import '../css/Footer.scss'
 
 const Footer = () => {
   return (
     <div className="footer-main-container">
       <div className="newsletter-container">
-      <h4>Sign up to our newsletter</h4>
-      <b>Email address</b>
+      <h4 id='signup'>Sign up to our newsletter</h4>
+      <p>Email address</p>
+      <div>
       <input type="text" className='newsletter-input'/>
+      </div>
+      <div>
       <button id='subscribe-btn'>Subscribe</button>
       </div>
+      </div>
       <div className="footer-nav-container">
-      <p>Services</p>
-      <p>Contact Us</p>
-      <p>About Us</p>
-      <p>Store</p>
+        <div>
+      <ul id='footmenu'>
+          <li id='footlink'>
+            <Link to="/services">Services</Link>
+          </li>
+          <li id='footlink'>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li id='footlink'>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li id='footlink'>
+            <Link to="/store">Store</Link>
+          </li>
+        </ul>
+        </div>
+        <div id='social-links'>
+        <i id='smicon' class="fa-brands fa-facebook fa-xl"></i>
+        <i id='smicon' class="fa-brands fa-twitter fa-xl"></i>
+        <i id='smicon' class="fa-brands fa-youtube fa-xl"></i>
+        <i id='smicon' class="fa-brands fa-instagram fa-xl"></i>
+        </div>
       </div>
       <div className="logo-container">
-      <img src={Logo} alt="Youthline Logo" />
+      <img id='ytflogo' src={Logo} alt="Youthline Logo" />
       </div>
     </div>
   )
